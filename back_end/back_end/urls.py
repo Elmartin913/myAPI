1832a9c419caa20b03e8cca3462e3dfa3bb306ad
Marcin Jablonowski <marcin.jab@wp.pl>
2017-11-29 22:48:22 +0100

    Models

--- back_end/back_end/urls.py
+++ back_end/back_end/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url
 from django.contrib import admin
-from rest.views import MoviesListView
+
+#from rest.views import MovieListView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', MoviesListView.as_view()),
-
+    #url(r'^movies/$', MovieListView.as_view())
 ]
--- back_end/rest/models.py
+++ back_end/rest/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 
-# Create your models here.
 
 class Person(models.Model):
-    name = models.CharField(max_length=128)
-    surname = models.CharField(max_length=128, null=True)
-    task = models.CharField(max_length=128, null=True)
+    name = models.CharField(max_length=255)
+
 
 class Movie(models.Model):
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=255)
     description = models.TextField()
-    director = models.ForeignKey(Person, on_delete=models.CASCADE)
-    actors = models.ManyToManyField(Person, related_name='actors')
-    year = models.ImageField(null=True)
+    director = models.ForeignKey(Person, related_name="movies_directed")
+    year = models.SmallIntegerField()
+    actors = models.ManyToManyField(Person, related_name="movies_cast")
